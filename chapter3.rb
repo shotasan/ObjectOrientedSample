d25b53d77d66c70227aa7d47103b36eb27b5e636
shotasan <wondermoai@gmail.com>
2019-11-17 19:57:41 +0900

    インスタンス作成の分離（１）

--- chapter3.rb
+++ chapter3.rb
@@ -1,12 +1,12 @@
 require 'byebug'
 
 class Gear
-  attr_reader :chainring, :cog, :wheel
+  attr_reader :chainring, :cog, :rim, :tire, :wheel
 
-  def initialize(chainring, cog, wheel)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
-    @wheel = wheel
+    @wheel = Wheel.new(rim, tire)
   end
 
   def gear_inches
@@ -31,4 +31,4 @@ class Wheel
   end
 end
 
-puts Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches
+puts Gear.new(52, 11, 26, 1.5).gear_inches
