2b3bede56e394b829f1e871d5c3ff90edfc39b19
shotasan <wondermoai@gmail.com>
2019-11-17 20:00:29 +0900

    インスタンス作成の分離（２）

--- chapter3.rb
+++ chapter3.rb
@@ -1,12 +1,18 @@
 require 'byebug'
 
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire, :wheel
+  attr_reader :chainring, :cog, :rim, :tire
 
   def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
-    @wheel = Wheel.new(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  # 必要な時だけWheeインスタンスが作成される
+  def wheel
+    @wheel ||= Wheel.new(rim, tire)
   end
 
   def gear_inches
