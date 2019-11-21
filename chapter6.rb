d40dae25d30492c99a50456ca8fae498e08c2ce1
shotasan <wondermoai@gmail.com>
2019-11-21 12:28:10 +0900

    テンプレートメソッド

--- chapter6.rb
+++ chapter6.rb
@@ -1,9 +1,29 @@
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :size, :chain, :tire_size
 
+  # default_tire_sizeという構造をスーパークラスで定義することで、サブクラスでのオーバーライドを可能にする
+  # 全ての時点スヤがチェーンには同じ初期値を持ち、タイヤサイズには異なる初期値をもつ
   def initialize(args)
     @size = args[:size]
+    @chain = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+  end
+
+  def default_chain
+    "10-speed"
+  end
+end
+
+class RoadBike < Bicycle
+  attr_reader :tape_color
+
+  def initialize(args)
     @tape_color = args[:tape_color]
+    super(args)
+  end
+
+  def default_tire_size
+    "23"
   end
 
   def spares
@@ -20,19 +40,32 @@ class MountainBike < Bicycle
     super(args)
   end
 
+  def default_tire_size
+    "2.1"
+  end
+
   def spares
     super.merge(rear_shock: rear_shock)
   end
 end
+
+road_bike = RoadBike.new(
+  size: "M",
+  tape_color: "red",
+  tire_size: "40",
+)
+
+p road_bike.size
+p road_bike.spares
+p road_bike.tire_size
+p road_bike.chain
 
 mountain_bike = MountainBike.new(
   size: "S",
   front_shock: "Manitou",
   rear_shock: "Fox",
 )
-p mountain_bike.size
-p mountain_bike.spares
 
-bike = Bicycle.new(size: "M", tape_color: "red")
-p bike.size
-p bike.spares
+p mountain_bike.size
+p mountain_bike.tire_size
+p mountain_bike.chain
