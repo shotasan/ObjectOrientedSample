7a4e0ff82034440e9039fdec1845c7eb75b1cb42
shotasan <wondermoai@gmail.com>
2019-11-21 12:12:00 +0900

    誤った継承関係

--- chapter6.rb
+++ chapter6.rb
@@ -11,6 +11,28 @@ class Bicycle
   end
 end
 
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
+mountain_bike = MountainBike.new(
+  size: "S",
+  front_shock: "Manitou",
+  rear_shock: "Fox",
+)
+p mountain_bike.size
+p mountain_bike.spares
+
 bike = Bicycle.new(size: "M", tape_color: "red")
 p bike.size
 p bike.spares
