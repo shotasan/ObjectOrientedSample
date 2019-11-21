9a3274bb043c5aa09afd5b5ce536e0d1c10745c8
shotasan <wondermoai@gmail.com>
2019-11-21 17:45:37 +0900

    フックメッセージを使用して結合度を低減

--- chapter6.rb
+++ chapter6.rb
@@ -1,19 +1,36 @@
+require "byebug"
+
 class Bicycle
   attr_reader :size, :chain, :tire_size
 
-  # default_tire_sizeという構造をスーパークラスで定義することで、サブクラスでのオーバーライドを可能にする
-  # 全ての時点スヤがチェーンには同じ初期値を持ち、タイヤサイズには異なる初期値をもつ
   def initialize(args)
     @size = args[:size]
     @chain = args[:chain] || default_chain
     @tire_size = args[:tire_size] || default_tire_size
+
+    # サブクラス独自の属性を取得
+    # サブクラスではinitializeが不要になる
+    post_initialize(args)
+  end
+
+  # フックメッセージ
+  def post_initialize(args)
+    nil
+  end
+
+  def spares
+    # サブクラスでオーバーライドされたlocal_sparesによりハッシュが返される
+    { tire_size: tire_size, chain: chain }.merge(local_spares)
+  end
+
+  def local_spares
+    {}
   end
 
   def default_chain
     "10-speed"
   end
 
-  # 追加されるクラスに関してもdefault_tire_sizeの実装を要求する
   def default_tire_size
     raise NotImplementedError,
       "this #{self.class} cannot respond to"
@@ -23,17 +40,17 @@ end
 class RoadBike < Bicycle
   attr_reader :tape_color
 
-  def initialize(args)
+  # Bicycleのメソッドをオーバーライド
+  def post_initialize(args)
     @tape_color = args[:tape_color]
-    super(args)
   end
 
   def default_tire_size
     "23"
   end
 
-  def spares
-    { chain: "10-speed", tire_size: "23", tape_color: tape_color }
+  def local_spares
+    { tape_color: tape_color }
   end
 end
 
@@ -50,16 +67,34 @@ class MountainBike < Bicycle
     "2.1"
   end
 
-  def spares
-    super.merge(rear_shock: rear_shock)
+  def local_spares
+    { front_shock: front_shock, rear_shock: rear_shock }
   end
 end
 
-# default_tire_sizeを定義していない
 class RecumbentBike < Bicycle
+  attr_reader :flag
+
+  def post_initialize(args)
+    @flag = args[:flag]
+  end
+
+  def local_spares
+    { flag: flag }
+  end
+
   def default_chain
     "9-speed"
   end
+
+  def default_tire_size
+    "28"
+  end
 end
 
-bent = RecumbentBike.new(size: "L")
+road_bike = RoadBike.new(size: "S", tape_color: "Blue")
+p road_bike.size
+p road_bike.spares
+
+bent = RecumbentBike.new(flag: "tall and orange")
+p bent.spares
