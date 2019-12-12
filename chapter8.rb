0704e2fb6ce1b65c53f055a78e6fd95f51eccd73
shotasan <wondermoai@gmail.com>
2019-12-12 15:26:29 +0900

    partsをpartに細分化する

--- chapter8.rb
+++ chapter8.rb
@@ -15,75 +15,37 @@ class Bicycle
 end
 
 class Parts
-  attr_reader :chain, :tire_size
+  attr_reader :parts
 
-  def initialize(args = {})
-    @chain = args[:chain] || default_chain
-    @tire_size = args[:tire_size] || default_tire_size
-    pots_initialize(args)
+  def initialize(parts)
+    @parts = parts
   end
 
   def spares
-    { tire_size: tire_size, chain: chain }.merge(local_spares)
-  end
-
-  def default_tire_size
-    raise NotImplementedError
-  end
-
-  def pots_initialize(args)
-    nil
-  end
-
-  def local_spares
-    {}
-  end
-
-  def default_chain
-    "10-speed"
+    parts.select { |part| part.needs_spare }
   end
 end
 
-class RoadBikeParts < Parts
-  attr_reader :tape_color
+class Part
+  attr_reader :name, :description, :needs_spare
 
-  def pots_initialize(args)
-    @tape_color = args[:tape_color]
-  end
-
-  def local_spares
-    { tape_color: tape_color }
-  end
-
-  def default_tire_size
-    "23"
+  def initialize(args)
+    @name = args[:name]
+    @description = args[:description]
+    @needs_spare = args.fetch(:needs_spare, true)
   end
 end
 
-class MountainBikeParts < Parts
-  attr_reader :front_shock, :rear_shock
+chain = Part.new(name: "chain", description: "10-speed")
+road_tire = Part.new(name: "tire_size", description: "23")
+tape = Part.new(name: "tape_color", description: "red")
+mountain_tire = Part.new(name: "tire_size", description: "2.1")
+rear_shock = Part.new(name: "rear_shock", description: "Fox")
+front_shock = Part.new(name: "front_shock", description: "Manitou", needs_spare: false)
 
-  def pots_initialize(args)
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
-  end
+# partの組み合わせでpartsを作成する
+road_bike_parts = Parts.new([chain, road_tire, tape])
+road_bike = Bicycle.new(size: "L", parts: road_bike_parts)
 
-  def local_spares
-    { rear_shock: rear_shock }
-  end
-
-  def default_tire_size
-    "2.1"
-  end
-end
-
-byebug
-road_bike = Bicycle.new(size: "L", parts: RoadBikeParts.new(tape_color: "red"))
-
-p road_bike.size
-p road_bike.spares
-
-mountain_bike = Bicycle.new(size: "L", parts: MountainBikeParts.new(rear_shock: "Fox"))
-
-p mountain_bike.size
-p mountain_bike.spares
+p road_bike.spares.size
+p road_bike.parts.size
