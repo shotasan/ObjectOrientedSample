f5e416d50197e5b81eac3183e2c9933086b6aa19
shotasan <wondermoai@gmail.com>
2019-12-13 11:22:17 +0900

    First commit

---
+++ test_chapter9.rb
@@ -0,0 +1,28 @@
+require "minitest/unit"
+require "minitest/autorun"
+require "./chapter9.rb"
+
+class WheelTest < MiniTest::Unit::TestCase
+  def test_calculates_diameter
+    wheel = Wheel.new(26, 1.5)
+
+    # assert_in_delta(expected, actual, delta = 0.001, message = nil) -> true
+    # 期待値と実際の値の差の絶対値が与えられた絶対誤差以下である場合、検査にパスしたことになります。
+    assert_in_delta(29, wheel.diameter, 0.01)
+  end
+end
+
+class GearTest < MiniTest::Unit::TestCase
+  def test_calculates_gear_inches
+    gear = Gear.new(
+      chainring: 52,
+      cog: 11,
+      rim: 26,
+      tire: 1.5,
+    )
+
+    # gear_inchesメソッドにはWheelへの依存があるがテストには現れない
+    # Geerを単体でテストできていない場合には設計に問題がある
+    assert_in_delta(137.1, gear.gear_inches, 0.01)
+  end
+end
